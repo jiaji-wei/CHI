f243f50e303096f54fab98588db994763cf57431
raytlty <alexmao930918@gmail.com>
2021-07-07 17:43:31 +0800

    update

--- scripts/deploy-rinkeby.ts
+++ scripts/deploy-rinkeby.ts
@@ -1,6 +1,6 @@
 import { ethers } from 'hardhat';
 
-const YangNFT = '0xd1309B94DAcA28bc402694a60Aee53089cfff5E5';
+const YangNFT = '0x0E1d4ac1F1858135b02F64CF6d47caEa5B218BAA';
 const MerkleRoot = '0xdb45131226a82a3ac77bac89d823bed43d130e9cb50cd0f03c84f6d28264a78f';
 const UniV3Factory = '0x1F98431c8aD98523631AE4a59f267346ea31F984';
 
@@ -20,11 +20,11 @@ async function main() {
     await CHIManager.deployed();
 
     console.log('CHIVaultDeployer:')
-    console.log(CHIDeployer.address) // 0x7eE5DaE59b06F8174D6Fd043dBC7e894b38D4860
+    console.log(CHIDeployer.address) // 0xd5C3B2e0B466601E2cCFC2f5D5A2E1e3d0950074
     console.log(CHIDeployer.deployTransaction.hash);
 
     console.log('CHIManager')
-    console.log(CHIManager.address) // 0x53CcaFA63368BD8D5162e00Ff116c2F6626944A4
+    console.log(CHIManager.address) // 0x7BA99E11f8A5bF1ca086B38795F852B9564D65E1
     console.log(CHIManager.deployTransaction.hash);
 
     await CHIDeployer.setCHIManager(CHIManager.address)
